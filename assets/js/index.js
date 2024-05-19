f32722a83747903b61a570454e78c543f35a840e
Lucas Gatto <lucasgatto17@gmail.com>
2024-05-19 16:45:10 -0300

    commit12

--- assets/js/index.js
+++ assets/js/index.js
@@ -63,6 +63,36 @@ function contactFormValidations() {
 
 
 
+// document.addEventListener("DOMContentLoaded", (e) => {
+//     contactFormValidations();
+// })
+
+
+
+
+// MENU HAMBURGUESA -------------------------------
+
+
+function hamburguerMenu(panelBtn, panel, menu) {
+    const d = document;
+
+    d.addEventListener("click", (e) => {
+        if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
+            d.querySelector(panel).classList.toggle("is-active");
+            d.querySelector(panelBtn).classList.toggle("is-active");
+        }
+        if (e.target.matches(menu)) {
+            d.querySelector(panel).classList.remove("is-active");
+            d.querySelector(panelBtn).classList.remove("is-active");
+        }
+    });
+}
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", (e) => {
+    hamburguerMenu(".panel-btn", ".panel", ".menu-hambur a");
     contactFormValidations();
-})
+});
